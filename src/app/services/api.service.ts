4308b880468f025285b10de6770c7316dbf73bb6
Arvin Frondarina <arvinfrondarina@Arvins-MacBook-Air.local>
2022-09-21 21:47:25 +0800

    updated the api uris

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,18 +9,18 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   postProduct(data : any){
-    return this.http.post<any>("http://localhost:3000/productList",data);
+    return this.http.post<any>("http://localhost:8080/api/v1/product",data);
   }
 
   getProduct(){
-    return this.http.get<any>("http://localhost:3000/productList");
+    return this.http.get<any>("http://localhost:8080/api/v1/products");
   }
 
   putProduct(data:any, id : number){
-    return this.http.put<any>("http://localhost:3000/productList/"+id, data);
+    return this.http.put<any>("http://localhost:8080/api/v1/product/"+id, data);
   }
 
   deleteProduct(id: number){
-    return this.http.delete<any>("http://localhost:3000/productList/"+id);
+    return this.http.delete<any>("http://localhost:8080/api/v1/product/"+id);
   }
 }
